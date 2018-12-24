87ccc2bb51a62a8cb25b3de3e6277094574001d5
ssingularity <1755405701@qq.com>
2018-12-24 21:17:57 +0800

    initialize the proj

---
+++ src/main/java/cn/sjtu/meetingroom/meetingroomcore/MeetingroomCoreApplication.java
@@ -0,0 +1,14 @@
+package cn.sjtu.meetingroom.meetingroomcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingroomCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeetingroomCoreApplication.class, args);
+    }
+
+}
+
