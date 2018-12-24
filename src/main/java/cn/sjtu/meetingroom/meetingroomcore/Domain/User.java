84673e5a9675e1b83bd3c3146ca3ae10a6225a5a
ssingularity <1755405701@qq.com>
2018-12-25 01:55:29 +0800

    finish pojo User

--- src/main/java/cn/sjtu/meetingroom/meetingroomcore/Domain/User.java
+++ src/main/java/cn/sjtu/meetingroom/meetingroomcore/Domain/User.java
@@ -1,5 +1,6 @@
 package cn.sjtu.meetingroom.meetingroomcore.Domain;
 
+import cn.sjtu.meetingroom.meetingroomcore.Util.Type;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -8,6 +9,9 @@ public class User {
     @Id
     String id;
     String name;
+    Type type;
+    Byte[] faceFile;
+    Byte[] featureFile;
 
     public String getId() {
         return id;
@@ -23,5 +27,29 @@ public class User {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public Byte[] getFaceFile() {
+        return faceFile;
+    }
+
+    public void setFaceFile(Byte[] faceFile) {
+        this.faceFile = faceFile;
+    }
+
+    public Byte[] getFeatureFile() {
+        return featureFile;
+    }
+
+    public void setFeatureFile(Byte[] featureFile) {
+        this.featureFile = featureFile;
     }
 }
---
+++ src/main/java/cn/sjtu/meetingroom/meetingroomcore/Util/Type.java
@@ -0,0 +1,12 @@
+package cn.sjtu.meetingroom.meetingroomcore.Util;
+
+public enum Type {
+    //user type
+    ORDINARY,
+    SUPERIORY,
+    ADMINISTOR,
+
+    //meeting type
+    COMMON,
+    URGECY
+}
