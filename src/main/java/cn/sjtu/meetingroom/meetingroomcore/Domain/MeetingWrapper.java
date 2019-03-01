af29b57842cbd061e0031e493e3fdb1b9f6a9417
ssingularity <1755405701@qq.com>
2019-03-01 20:40:22 +0800

    modify meeting wrapper add attendant names

--- src/main/java/cn/sjtu/meetingroom/meetingroomcore/Domain/MeetingWrapper.java
+++ src/main/java/cn/sjtu/meetingroom/meetingroomcore/Domain/MeetingWrapper.java
@@ -5,12 +5,20 @@ import cn.sjtu.meetingroom.meetingroomcore.Service.UserService;
 import io.swagger.annotations.ApiModelProperty;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MeetingWrapper extends Meeting {
 
     public static MeetingWrapper create(Meeting meeting, UserService userService, MeetingRoomService meetingRoomService, String errorNum){
         MeetingWrapper res = new MeetingWrapper(meeting);
+        Map<String, String> map = new HashMap<>();
+        for (Map.Entry<String, String> entry : meeting.getAttendants().entrySet()){
+            User user = userService.showOne(entry.getKey());
+            map.put(user.getName(), entry.getValue());
+        }
+        res.setAttendantsName(map);
         res.setHost(userService.showOne(meeting.getHostId()));
         res.setLocation(meetingRoomService.findById(meeting.getRoomId()).getLocation());
         res.setErrorNum(errorNum);
@@ -30,6 +38,7 @@ public class MeetingWrapper extends Meeting {
     private User host;
     @ApiModelProperty(notes = "200 delegates ok , 400 delegates fail")
     private String errorNum;
+    private Map<String, String> attendantsName;
 
     public String getErrorNum() {
         return errorNum;
@@ -43,6 +52,14 @@ public class MeetingWrapper extends Meeting {
         super(meeting.id, meeting.heading, meeting.description, meeting.roomId, meeting.date, meeting.location,
                 meeting.startTime, meeting.endTime, meeting.hostId, meeting.attendants, meeting.needSignIn,
                 meeting.attendantNum, meeting.status, meeting.type, meeting.tags, meeting.timestamp, meeting.foreignGuestList);
+    }
+
+    public Map<String, String> getAttendantsName() {
+        return attendantsName;
+    }
+
+    public void setAttendantsName(Map<String, String> attendantsName) {
+        this.attendantsName = attendantsName;
     }
 
     public User getHost() {
